a1dba9b68f905868a3d47fd9ad9e8a97406539d4
dylann97 <d.xavero@hotmail.com>
2024-08-05 19:46:27 +0200

    🚧  F01: Build Testimonials section

---
+++ components/Testimonials.tsx
@@ -0,0 +1,74 @@
+import { cn } from "@/lib/utils";
+import Marquee from "@/components/magicui/marquee";
+import { reviews } from "@/data";
+import Title from "./ui/Title";
+
+const firstRow = reviews.slice(0, reviews.length / 3);
+const secondRow = reviews.slice(reviews.length / 3, reviews.length / 1.5);
+const thirdRow = reviews.slice(reviews.length / 1.5);
+
+const ReviewCard = ({
+  img,
+  name,
+  username,
+  body,
+}: {
+  img: string;
+  name: string;
+  username: string;
+  body: string;
+}) => {
+  return (
+    <figure
+      className={cn(
+        "relative h-40 w-36 cursor-pointer overflow-hidden rounded-xl border p-4",
+        // light styles
+        "border-gray-950/[.1] bg-gray-950/[.01] hover:bg-gray-950/[.05]",
+        // dark styles
+        "dark:border-gray-50/[.1] dark:bg-gray-50/[.10] dark:hover:bg-gray-50/[.15]"
+      )}
+    >
+      <div className="flex flex-row items-center gap-2">
+        <img className="rounded-full" width="32" height="32" alt="" src={img} />
+        <div className="flex flex-col">
+          <figcaption className="text-sm font-medium dark:text-white">
+            {name}
+          </figcaption>
+          <p className="text-xs font-medium dark:text-white/40">{username}</p>
+        </div>
+      </div>
+      <blockquote className="mt-2 text-sm">{body}</blockquote>
+    </figure>
+  );
+};
+
+export function Testimonials() {
+  return (
+    <section className="mb-32">
+      <Title
+        level="h2"
+        label="Ils ont transformé leur vie avec nous"
+        className="mb-16"
+      />
+      <div className="relative flex h-[500px] w-full flex-row items-center justify-center overflow-hidden">
+        <Marquee pauseOnHover vertical className="[--duration:35s]">
+          {firstRow.map((review) => (
+            <ReviewCard key={review.username} {...review} />
+          ))}
+        </Marquee>
+        <Marquee pauseOnHover vertical className="[--duration:25s]">
+          {secondRow.map((review) => (
+            <ReviewCard key={review.username} {...review} />
+          ))}
+        </Marquee>
+        <Marquee pauseOnHover vertical className="[--duration:35s]">
+          {thirdRow.map((review) => (
+            <ReviewCard key={review.username} {...review} />
+          ))}
+        </Marquee>
+        <div className="pointer-events-none absolute inset-x-0 top-0 h-1/3 bg-gradient-to-b from-[#f5f5f5] dark:from-background"></div>
+        <div className="pointer-events-none absolute inset-x-0 bottom-0 h-1/3 bg-gradient-to-t from-[#f5f5f5] dark:from-background"></div>
+      </div>
+    </section>
+  );
+}
