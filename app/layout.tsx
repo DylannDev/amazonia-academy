00a21c3fdea638afc5902ef5a62f7a640ed66d12
dylann97 <d.xavero@hotmail.com>
2024-08-05 19:33:11 +0200

    💄 F01: Change background color of body

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Navbar from "../components/Navbar";
+import clsx from "clsx";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -18,8 +19,8 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>
-        <div className="max-w-6xl mx-auto">
+      <body className={(inter.className, clsx("bg-[#f5f5f5] "))}>
+        <div className="max-w-6xl mx-auto px-5 lg:px-10">
           <Navbar />
           {children}
         </div>
