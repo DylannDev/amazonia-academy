4af6ca97fcc4477ef874b809493df1d73495562f
dylann97 <d.xavero@hotmail.com>
2024-08-21 20:41:43 +0200

    📝 Create new file to export the types

---
+++ app/types/blog.ts
@@ -0,0 +1,11 @@
+export interface BlogArticle {
+  id: number;
+  title: string;
+  subtitle: string;
+  content: string;
+  date: string;
+  author: string;
+  categories: string[];
+  image: string;
+  readTime: string;
+}
